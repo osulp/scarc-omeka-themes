3780c12af175ef1f4651619c0438f5973a245db2
Chris Petersen <chris.petersen@oregonstate.edu>
2018-06-08 16:03:41 -0700

    Modified the Waldo Hall Omeka theme for use in building the Womens Words Omeka site.

---
+++ womenswords/exhibit-builder/exhibits/show.php
@@ -0,0 +1,31 @@
+<?php 
+echo head(array(
+    'title' => metadata('exhibit_page', 'title') . ' &middot; ' . metadata('exhibit', 'title'),
+    'bodyclass' => 'exhibits show'));
+?>
+
+
+<nav id="exhibit-child-pages">
+    <?php echo exhibit_builder_lower_page_nav(); ?>
+</nav>
+
+
+<h1><span class="exhibit-page"><?php echo metadata('exhibit_page', 'title'); ?></span></h1>
+
+
+<div id="primary">
+
+	<div id="previous-next-nav">
+		<div class="previous"><?php echo exhibit_builder_link_to_previous_page(); ?></div>
+		<div class="next"><?php echo exhibit_builder_link_to_next_page(); ?></div>
+	</div>
+
+	<div class="exhibit-content">
+		<?php exhibit_builder_render_exhibit_page(); ?>
+	</div>
+	<div id="previous-next-nav">
+		<div class="previous"><?php echo exhibit_builder_link_to_previous_page(); ?></div>
+		<div class="next"><?php echo exhibit_builder_link_to_next_page(); ?></div>
+	</div>
+</div><!--end primary-->
+<?php echo foot(); ?>
